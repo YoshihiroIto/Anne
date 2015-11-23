2464d40b65e7a18295c3333d5628b685044885ed
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2015-11-23 22:44:05 +0900

    Add nuget

--- Anne/MainWindow/MainWindowVm.cs
+++ Anne/MainWindow/MainWindowVm.cs
@@ -1,9 +1,16 @@
 ﻿using Livet;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace Anne.MainWindow
 {
     public class MainWindowVm : ViewModel
     {
-         
+        public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>("Anne");
+
+        public MainWindowVm()
+        {
+            Title.AddTo(MultipleDisposable);
+        }
     }
 }
