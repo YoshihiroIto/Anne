be6937939768953c64d60799fb38b1dedf8b5881
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2015-12-21 14:51:29 +0900

    Repositry クラス用意

---
+++ Anne/Features/RepositoryView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Features
+{
+    /// <summary>
+    /// RepositoryView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RepositoryView
+    {
+        public RepositoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
