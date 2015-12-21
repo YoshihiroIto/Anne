05c6d42ab506b6e1ee1a70d285dd377340c53ec5
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2015-12-21 14:22:59 +0900

    DisposableChecker を Debugビルドのみ有効にするようにした

--- Anne.Foundation/DisposableChecker.cs
+++ Anne.Foundation/DisposableChecker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Diagnostics;
 
 namespace Anne.Foundation
 {
@@ -9,10 +10,12 @@ namespace Anne.Foundation
     {
         private static readonly HashSet<IDisposable> Disposables = new HashSet<IDisposable>();
 
+        [Conditional("DEBUG")]
         public static void Start()
         {
         }
 
+        [Conditional("DEBUG")]
         public static void End()
         {
             if (Disposables.Any())
@@ -21,11 +24,13 @@ namespace Anne.Foundation
             }
         }
 
+        [Conditional("DEBUG")]
         public static void Add(IDisposable disposable)
         {
             Disposables.Add(disposable);
         }
 
+        [Conditional("DEBUG")]
         public static void Remove(IDisposable disposable)
         {
 #if false
