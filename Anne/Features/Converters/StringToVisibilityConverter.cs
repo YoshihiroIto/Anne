7f801cdd75cb63054cff0dc18d07680ad982ceee
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2015-12-30 13:07:19 +0900

    ウォーターマーク実装
    * コミットリスト
    * コミット入力ビュー

---
+++ Anne/Features/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Anne.Features.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public Visibility WhiteSpace { get; set; }
+        public Visibility NoWhiteSpace { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = value as string;
+            if (s == null)
+                return WhiteSpace;
+
+            return string.IsNullOrWhiteSpace(s) ? WhiteSpace : NoWhiteSpace;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
