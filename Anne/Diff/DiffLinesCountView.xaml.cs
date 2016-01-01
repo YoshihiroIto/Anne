f7d880f5952f7217bbd84d200b135d6d46635cdb
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-01 17:11:32 +0900

    追加削除行数表示コントロール実装

---
+++ Anne/Diff/DiffLinesCountView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Diff
+{
+    /// <summary>
+    /// DiffLinesCountView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DiffLinesCountView
+    {
+        public DiffLinesCountView()
+        {
+            InitializeComponent();
+        }
+    }
+}
