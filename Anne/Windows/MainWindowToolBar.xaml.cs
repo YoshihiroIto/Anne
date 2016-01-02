e4d86309ef6fd22aef3aab47f0d1ea4285aa5cde
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-02 12:21:46 +0900

    メインウィンドウ見た目調整

---
+++ Anne/Windows/MainWindowToolBar.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Windows
+{
+    /// <summary>
+    /// MainWindowToolBar.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindowToolBar
+    {
+        public MainWindowToolBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
