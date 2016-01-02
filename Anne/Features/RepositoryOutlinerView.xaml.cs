b1cce8a4608e4f96f746ab9754704ce030548533
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-02 15:56:57 +0900

    アウトライナー追加

---
+++ Anne/Features/RepositoryOutlinerView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Features
+{
+    /// <summary>
+    /// RepositoryOutlinerView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RepositoryOutlinerView
+    {
+        public RepositoryOutlinerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
