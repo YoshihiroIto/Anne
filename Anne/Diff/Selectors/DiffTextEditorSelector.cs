d5d84156af1a67a9656ba513c3b8b8d36cbb80fb
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-04 16:53:32 +0900

    バイナリファイルは比較ビューを表示しないようにした

---
+++ Anne/Diff/Selectors/DiffTextEditorSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Anne.Features.Interfaces;
+
+namespace Anne.Diff.Selectors
+{
+    public class DiffTextEditorSelector : DataTemplateSelector
+    {
+        public DataTemplate Ascii { get; set; }
+        public DataTemplate Binary { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var fileDiff = item as IFileDiffVm;
+            if (fileDiff == null)
+                return null;
+
+            return fileDiff.IsBinary ? Binary : Ascii;
+        } 
+    }
+}
