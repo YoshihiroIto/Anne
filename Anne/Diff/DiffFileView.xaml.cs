633a8453a2203128a8b8a765c2f3c2e8df08bb74
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-06 23:59:54 +0900

    DiffFileView, DiffFileViewList再実装

---
+++ Anne/Diff/DiffFileView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Diff
+{
+    /// <summary>
+    /// DiffFileView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DiffFileView
+    {
+        public DiffFileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
