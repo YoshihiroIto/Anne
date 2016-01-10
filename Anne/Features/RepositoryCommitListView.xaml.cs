e43dd0a7a160e68efc40dc19334e4a107906a94a
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-10 14:48:47 +0900

    コミットリストをリポジトリビューから分離

---
+++ Anne/Features/RepositoryCommitListView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Anne.Features
+{
+    /// <summary>
+    /// RepositoryCommitListView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RepositoryCommitListView
+    {
+        public RepositoryCommitListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
