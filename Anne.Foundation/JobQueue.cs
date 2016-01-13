6373db31d078868c592a06f0dae58500d86ca7d9
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-13 22:55:57 +0900

    JobQueue再実装

--- Anne.Foundation/JobQueue.cs
+++ Anne.Foundation/JobQueue.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reactive.Concurrency;
-using System.Threading;
 using System.Threading.Tasks;
 using Reactive.Bindings;
 using StatefulModel;
@@ -17,106 +18,78 @@ namespace Anne.Foundation
         public ReactiveProperty<string> WorkingJob { get; } =
             new ReactiveProperty<string>(Scheduler.Immediate);
 
-        private readonly ObservableSynchronizedCollection<Job> _jobs =
-            new ObservableSynchronizedCollection<Job>();
-
-        private readonly Task _task;
-        private readonly SemaphoreSlim _sema = new SemaphoreSlim(0, 1);
-        private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
-
         private class Job
         {
             public string Summry { get; set; }
             public Action Action { get; set; }
         }
 
-        public void Dispose()
-        {
-            try
-            {
-                _cancellationToken.Cancel();
-                _sema.Release();
-                _task.Wait();
-            }
-            catch (AggregateException)
-            {
-                Debug.WriteLine("Task is cancelled.");
-            }
+        private readonly Queue<Job> _jobs = new Queue<Job>();
+        private bool _isActive;
 
-            _cancellationToken.Dispose();
-            _sema.Dispose();
-
-            _task.Dispose();
-
-            JobSummries.Dispose();
-        }
-
-        public JobQueue()
-        {
-
-// todo: _jobsから作成すると例外が起きてします。一旦処理しないようにする。要調査
-#if false
-            JobSummries = _jobs
-                .ToReadOnlyReactiveCollection(
-                    _jobs.ToCollectionChanged<Job>(),
-                    x => x.Summry,
-                    Scheduler.Immediate);
-#endif
-
-            _task = Task.Run(() =>
-            {
-                while (true)
-                {
-                    _cancellationToken.Token.ThrowIfCancellationRequested();
-                    _sema.Wait();
-
-                    while (_jobs.Count > 0)
-                    {
-                        _cancellationToken.Token.ThrowIfCancellationRequested();
-
-                        var job = _jobs[0];
-                        _jobs.RemoveAt(0);
-
-                        using (new AnonymousDisposable(() => WorkingJob.Value = string.Empty))
-                        {
-                            WorkingJob.Value = job.Summry;
-
-                            try
-                            {
-                                job.Action();
-                            }
-                            catch(Exception e)
-                            {
-                                var args = new ExceptionEventArgs { Exception = e, Summry = job.Summry};
-
-                                JobExecutingException?.Invoke(this, args);
-
-                                // 例外が起きたので以降のジョブは実行しない
-                                _jobs.Clear();
-                            }
-                        }
-                    }
-                }
-
-                // ReSharper disable once FunctionNeverReturns
-            }, _cancellationToken.Token);
-        }
+        private readonly object _syncObj = new object();
 
         public void AddJob(string summary, Action action)
         {
-            _jobs.Add(new Job
+            lock (_syncObj)
             {
-                Summry = summary,
-                Action = action
-            });
+                _jobs.Enqueue(new Job {Summry = summary, Action = action});
 
-            try
-            {
-                _sema.Release();
+                if (_isActive)
+                    return;
+
+                _isActive = true;
+                RunJob();
             }
-            catch (SemaphoreFullException)
+        }
+
+        private void RunJob()
+        {
+            Job job;
+
+            lock(_syncObj)
             {
+                if (_jobs.Any() == false)
+                {
+                    _isActive = false;
+                    return;
+                }
+
+                job = _jobs.Dequeue();
             }
+
+            Task.Run(() =>
+            {
+                using (new AnonymousDisposable(() => WorkingJob.Value = string.Empty))
+                {
+                    WorkingJob.Value = job.Summry;
+
+                    Debug.WriteLine("JobQueue >>>>>>>>>>>>>>>>>>>>>>>>>> " + job.Summry);
+
+                    try
+                    {
+                        job.Action();
+                    }
+                    catch (Exception e)
+                    {
+                        var args = new ExceptionEventArgs {Exception = e, Summry = job.Summry};
+
+                        JobExecutingException?.Invoke(this, args);
+
+                        // 例外が起きたので以降のジョブは実行しない
+                        lock (_syncObj)
+                        {
+                            _jobs.Clear();
+                        }
+                    }
+                }
+            }).ContinueWith(_ => RunJob());
+        }
+
+        public void Dispose()
+        {
+            WorkingJob.Dispose();
+            JobSummries.Dispose();
         }
     }
 }
