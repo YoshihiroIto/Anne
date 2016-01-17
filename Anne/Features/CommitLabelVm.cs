e0d6ad9df093d3a384ee446f330aa2867ed28c99
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-01-17 16:19:04 +0900

    CommitLabelVm軽量化

--- Anne/Features/CommitLabelVm.cs
+++ Anne/Features/CommitLabelVm.cs
@@ -2,33 +2,22 @@
 using System.Windows.Media;
 using Anne.Foundation;
 using Anne.Foundation.Mvvm;
-using Reactive.Bindings;
-using Reactive.Bindings.Extensions;
 
 namespace Anne.Features
 {
     public class CommitLabelVm : ViewModelBase
     {
-        public ReadOnlyReactiveProperty<string> Name { get; }
-        public ReadOnlyReactiveProperty<CommitLabelType> Type { get; }
-        public ReactiveProperty<Brush> Background { get; }
+        public string Name { get; }
+        public CommitLabelType Type { get; }
+        public Brush Background { get; }
 
         public CommitLabelVm(Model.Git.CommitLabel model)
         {
             Debug.Assert(model != null);
 
-            Name = model
-                .ObserveProperty(x => x.Name)
-                .ToReadOnlyReactiveProperty()
-                .AddTo(MultipleDisposable);
-
-            Type = model
-                .ObserveProperty(x => x.Type)
-                .ToReadOnlyReactiveProperty()
-                .AddTo(MultipleDisposable);
-
-            Background = new ReactiveProperty<Brush>(Brushes.MistyRose)
-                .AddTo(MultipleDisposable);
+            Name = model.Name;
+            Type = model.Type;
+            Background = Brushes.MistyRose;
         }
     }
 }
