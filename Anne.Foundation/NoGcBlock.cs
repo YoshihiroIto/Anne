b2ad0fc1ccb160b8dda97948f2b803ba552eae4e
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-02-01 00:12:56 +0900

    NoGcBlock, 再入を正しく扱えるようにした

--- Anne.Foundation/NoGcBlock.cs
+++ Anne.Foundation/NoGcBlock.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Runtime;
+using System.Threading;
 
 namespace Anne.Foundation
 {
     public class NoGcBlock : IDisposable
     {
+        private static int _depth;
+
         public NoGcBlock()
         {
             GC.TryStartNoGCRegion(128*1024*1024);
+
+            Interlocked.Increment(ref _depth);
         }
 
         public void Dispose()
         {
-            if (GCSettings.LatencyMode == GCLatencyMode.NoGCRegion)
-                GC.EndNoGCRegion();
+            Interlocked.Decrement(ref _depth);
+
+            if (_depth == 0)
+                if (GCSettings.LatencyMode == GCLatencyMode.NoGCRegion)
+                    GC.EndNoGCRegion();
         }
     }
 }
