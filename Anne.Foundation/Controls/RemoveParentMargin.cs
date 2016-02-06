d86106a196d678ae2fad8001d85c5174dc41e122
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-02-06 21:41:44 +0900

    コミットリストアイテムのマージンを処理するようにした

---
+++ Anne.Foundation/Controls/RemoveParentMargin.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Anne.Foundation.Controls
+{
+    // https://www.interact-sw.co.uk/iangblog/2007/05/30/wpf-listview-column-margins
+    public class RemoveParentMargin : Decorator
+    {
+        protected override void OnVisualParentChanged(DependencyObject oldParent)
+        {
+            base.OnVisualParentChanged(oldParent);
+
+            var cp = VisualTreeHelper.GetParent(this) as FrameworkElement;
+
+            if (cp != null)
+                cp.Margin = new Thickness(0.0);
+        }
+    }
+}
